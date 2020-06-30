b976cf067e4ccb71708b250a82de49c0adccf33e
akaseren <ren.akase@rakus.co.jp>
2020-06-30 15:27:38 +0900

    入力値エラー処理追加

--- src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jp.co.sample.emp_management.domain.Administrator;
 import jp.co.sample.emp_management.form.InsertAdministratorForm;
@@ -56,7 +58,7 @@ public class AdministratorController {
 	 * @return 管理者登録画面
 	 */
 	@RequestMapping("/toInsert")
-	public String toInsert() {
+	public String toInsert(Model model) {
 		return "administrator/insert";
 	}
 
@@ -65,14 +67,23 @@ public class AdministratorController {
 	 * 
 	 * @param form
 	 *            管理者情報用フォーム
-	 * @return ログイン画面へリダイレクト
+	 * @return ログイン画面へリダイレクト(エラー時は登録画面)
 	 */
 	@RequestMapping("/insert")
-	public String insert(InsertAdministratorForm form) {
+	public String insert(@Validated InsertAdministratorForm form
+			, BindingResult result
+			, RedirectAttributes redirectAttributes
+			, Model model) {
+		
+		if(result.hasErrors()) {
+			return toInsert(model);
+		}
+		
 		Administrator administrator = new Administrator();
 		// フォームからドメインにプロパティ値をコピー
 		BeanUtils.copyProperties(form, administrator);
 		administratorService.insert(administrator);
+		
 		return "redirect:/";
 	}
 
